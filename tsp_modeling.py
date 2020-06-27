75c5f11a070b6865ac10d62272c7e15a6158ee89
“Xinyi <xinyi.ye@kcl.ac.uk>
2020-06-27 19:49:54 +0100

    bug fixed. Implement subtour elimination constraint

--- tsp_modeling.py
+++ tsp_modeling.py
@@ -26,10 +26,6 @@ model.n = Param(within=PositiveIntegers)
 model.L = Param(model.C, within=Any)
 #the coordinates of the cities
 
-# model.c1 = Param(model.C)
-# #the starting city
-# model.D = Param(within=PositiveIntegers)
-#departture city
 
 model.M = RangeSet(model.n)
 model.N = RangeSet(model.n)
@@ -41,24 +37,8 @@ model.x = Var(model.N, model.M, within=Binary, initialize=0)
 
 
 
-model.s = Var(model.S, within=IntegerSet)
-
-
-#whether path x is selected 
-
-# model.k = Var(model.N,within=PositiveIntegers, bounds=(1,4))
-#the kth city being visited
-
-
 model.d = Param(model.N, model.M, initialize= lambda model, i, j : sqrt(((model.L[i][0]- model.L[j][0])**2) + ((model.L[i][1]- model.L[j][1])**2) ))
-#the direct distance between city i and j 
 
-# =============================================================================
-# def obj_expression(model):
-#     return sum(model.x[i, j] * model.d[i, j] for i in model.N for j in model.M)
-# 
-# model.OBJ = Objective(sense=minimize, rule=obj_expression)
-# =============================================================================
 def obj_func(model):
     return sum(model.x[i,j] * model.d[i,j] for i in model.N for j in model.M)
 
@@ -78,112 +58,80 @@ def obj_constraint_2(model, N):
 model.const_2 = Constraint(model.N,rule=obj_constraint_2)
 
 data = {None:{
-    'C':{None:[1,2,3,4,5]},
-    'L':{1:(0,0), 2:(0,2), 3:(1,1), 4:(2,0), 5:(2,2)},
-    'n':{None: 5}
+    'C':{None:[1,2,3,4,5,6]},
+    'L':{1:(0,0), 2:(0,2), 3:(1,1), 4:(2,0), 5:(2,2), 6:(1,4)},
+    'n':{None: 6}
     }}
 
 instance = model.create_instance(data)
 results = opt.solve(instance)
 
 
-instance.display()
+adj_city = {}
+tour  = set() #set of cities inside the subtour
+first = list(instance.C)[0] #the first city
+min_tour = [] #the smallest subtour
+size = len(tour) 
+u = set()
 
+def successor():
+    for i in instance.C:
+        adj_city[i] = sum(j*instance.x[i,j] for j in instance.C)
+        
+def getTour():
+    global first
+    global tour
+    global size
+    while True:
+        tour.add(first)
+        first = int(value(adj_city[first]))
+        if first == 1:
+            size = len(tour)
+            break
+            
+def get_min_tour():
+    global size
+    global tour
+    global min_tour
+    global u
+    if size < value(instance.n):
+        min_tour = tour
+        if size > 2:
+            u = tour
+            for i in instance.C:
+                if i not in u:
+                    tour = set()
+                    first = i
+                    while True:
+                        tour.add(first)
+                        first = int(value(adj_city[first]))
+                        if first == i:
+                            break
+                    u.update(tour)
+                    if size > len(tour):
+                        min_tour = tour
+                        size = len(min_tour)
+                    if size == 2:
+                        break
 
-# =============================================================================
-# 
-# next = [None]*(len(instance.C)+1)
-# tour  = []
-# first = list(instance.C)[0]
-# min_tour = []
-# size = len(tour)
-# u = set()
-# 
-# def successor():
-#     for i in instance.C:
-#         next[i] = sum(j*instance.x[i,j] for j in instance.C)
-#         
-# def getTour():
-#     global first
-#     while True:
-#         tour.append(first)
-#         first = int(value(next[first]))
-#         if first == 1:
-#             break
-#             
-# def get_min_tour():
-#     global size
-#     global tour
-#     if size < value(instance.n):
-#         min_tour = tour
-#         if size > 2:
-#             u = tour
-#             for i in instance.C:
-#                 if i not in u:
-#                     tour = []
-#                     first = 1
-#                     while True():
-#                         tour.append(first)
-#                         first = next(first)
-#                         if first == i:
-#                             break
-#                     u.add(tour)
-#                     if size > len(tour):
-#                         min_tour = tour
-#                         size = len(min_tour)
-#                     if size == 2:
-#                         break
-#                     
-# # =============================================================================
-# # 
-# # def const_3(model):
-# #     return sum(instance.x[i,next[i]] for i in instance.C ) <= len(min_tour) -1
-# # =============================================================================
-# successor()
-# getTour()
-# get_min_tour()
-#                       
-# 
-# instance.cuts.add(sum(value(instance.x[i,next[i]]) for i in instance.C) <= len(min_tour) -1)
-# results = opt.solve(instance)
-# print ("\n===== iteration")
-# 
-# instance.display()
-# 
-# 
-# for i in List:
-#     if instance.x[i]() != 0:
-#         print(i,'--', instance.x[i]())
-# =============================================================================
+successor()
+getTour()
+get_min_tour()
+                      
+expr = sum(instance.x[i,value(adj_city[i])] for i in min_tour)
 
-# =============================================================================
-# for i in range(5):
-#     expr = 0
-# 
-#     for j in instance.x:
-#         if value(instance.x[j]) == 0:
-#             expr += instance.x[j]
-#             
-#         else:
-#             expr += (1-instance.x[j])
-#             subtour.append(j)
-# 
-#     cons = sum(instance.x[d] for d in instance.s
-#     
-# 
-#     new_cons = sum(instance.x[d] for d in subtour)<= expr -1 
-#     instance.cuts.add(new_cons)
-#     results = opt.solve(instance)
-#     print ("\n===== iteration",i)
-#     List = list(instance.x.keys())
-#     for i in List:
-#         if instance.x[i]() != 0:
-#             print(i,'--', instance.x[i]())
-# =============================================================================
+instance.cuts.add(expr <= len(min_tour) -1)
+results = opt.solve(instance)
+List = list(instance.x.keys())
+for i in List:
+    if instance.x[i]() != 0:
+        print(i,'--', instance.x[i]())
+print(value(instance.objective))
+instance.cuts.display()
 
 
         
-
+'''subtour 是一个城市的集合，不是edge/path的集合'''
 
 '''如果存在subtour，是不是s的大小只要n/2就好了？？'''
 
@@ -201,6 +149,9 @@ instance.display()
 #      #x[i,j]means theres a path from i to j so k[i] is smaller than k[j] 
 #     
 # model.const_3 = Constraint(model.N, model.M, rule=obj_constraint_3)
+#
+#
+##using MTH algoirthm to eliminate the subtour
 # =============================================================================
 
 
