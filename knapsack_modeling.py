6211e8673018439ea82521968258cdb932b161f8
“Xinyi <xinyi.ye@kcl.ac.uk>
2020-07-02 13:21:25 +0100

    add test data

--- knapsack_modeling.py
+++ knapsack_modeling.py
@@ -6,7 +6,9 @@ model = AbstractModel()
 
 model.capacity = Param(within=NonNegativeIntegers)
 
-model.Item = Set()
+model.size = Param(within=PositiveIntegers)
+
+model.Item = RangeSet(model.size)
 
 model.value = Param(model.Item, within=PositiveIntegers)
 model.weight = Param(model.Item, within=PositiveIntegers)
@@ -14,7 +16,7 @@ model.weight = Param(model.Item, within=PositiveIntegers)
 model.x = Var(model.Item, within=Binary)
 
 def obj_expression(model):
-	return summation(model.value, model.x)
+	return sum(model.value[i]*model.x[i] for i in model.Item)
 
 model.OBJ = Objective(sense=maximize, rule=obj_expression)
 
@@ -27,29 +29,32 @@ model.AxbCOnstraint = Constraint(rule=ax_constraint_rule)
 ##TEST
 
 data_init_1 = {None: dict(
-        capacity = {None : 10},
-        Item = {None: ["Apple", "Pear", "Orange", "Strawberry"]},
-        value = {"Apple":4, "Pear":2,"Orange":3, "Strawberry":5},
-        weight = {"Apple":2, "Pear":3,"Orange":3, "Strawberry":4}
+        size = {None:6},
+        capacity = {None : 190},
+        # Item = {None: ["Apple", "Pear", "Orange", "Strawberry"]},
+        weight = {1 : 56 ,
+2 : 59 ,
+3 : 80 ,
+4 : 64 ,
+5 : 75 ,
+6 : 17},
+        value = {1 : 50 ,
+2 : 50 ,
+3 : 64 ,
+4 : 46 ,
+5 : 50 ,
+6 : 5 }
         )}
 
-data_init_2 = {None: dict(
-        capacity = {None : 10},
-        Item = {None: ["Cat", "Dog", "Snake", "Monkey", "Fish"]},
-        value = {"Cat":1, "Dog":2, "Snake":3, "Monkey":4, "Fish":1},
-        weight = {"Cat":2, "Dog":3, "Snake":2, "Monkey":5, "Fish":1}
-        )}
 
 
-
-instance_1 = model.create_instance(data_init_1)
-instance_2 = model.create_instance(data_init_2)
+instance = model.create_instance(data_init_1)
 
 
 
 if __name__ == '__main__':
     opt = SolverFactory('cplex',executable = '/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx/cplex')
-    results_1 = opt.solve(instance_1, tee=True)
-    results_2 = opt.solve(instance_2, tee=True)
+    results_1 = opt.solve(instance, tee=True)
     results_1.write()
     print("\n Solution: \n" + '-'*60)
+    instance.display()
